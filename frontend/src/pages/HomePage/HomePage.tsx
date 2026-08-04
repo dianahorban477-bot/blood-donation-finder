4183f8505d832a0b279a8e819031baa32bd83f42
Oksana Palahecha <oksanapalahecha@gmail.com>
2026-08-04 18:38:34 +0300

    chore(frontend): add initial setup

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react'
+
+export const HomePage: FC = () => {
+  return (
+    <main className="home-page">
+      <h1 className="home-page__title">Blood Donation Finder</h1>
+      <p className="home-page__description">
+        Frontend setup
+      </p>
+    </main>
+  )
+}
